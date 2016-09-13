76c8b03b9b828ba5e6b6ba5c1046c47695d15d7c
Kyle Rector <kylearector@outlook.com>
2016-09-13 09:41:10 -0400

    Removing old code

--- php/Test2.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $mes="010";
-        $retval=0;
-        $last_line=0;
-        $SndMsg = "/var/www/c/SendUART ".$mes;
-        $last_line = system($SndMsg, $retval);
-        header("Location:/../index.php");
-?>
