9db0ce8baf9a3d706d41e80701269447045d28b0
Kyle Rector <kylearector@outlook.com>
2016-09-20 20:14:38 -0400

    Partially complete Kinect Service

---
+++ c#/KinectService/KinectVoiceService.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Speech.AudioFormat;
+using Microsoft.Speech.Recognition;
+using System.IO;
+using System.Net;
+
+namespace KinectService
+{
+    public partial class KinectVoiceService : ServiceBase
+    {
+        public KinectVoiceService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            kinect.KinectStart();
+        }
+
+        protected override void OnStop()
+        {
+            kinect.KinectStop();
+        }
+
+        private KinectInterface kinect = new KinectInterface();
+    }
+
+    class KinectInterface
+    {
+        private void SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // Speech utterance confidence below which we treat speech as if it hadn't been heard
+            const double ConfidenceThreshold = 0.3;
+
+            if (e.Result.Confidence >= ConfidenceThreshold)
+            {
+                switch (e.Result.Semantics.Value.ToString())
+                {
+                    case "LAMP ON":
+                        if (called)
+                        {
+                            WebRequest request = WebRequest.Create("http://192.168.1.147/toggleSwitch?sensor=Table%20Lamp&state=True");
+                            WebResponse response = request.GetResponse();
+                            called = false;
+                        }
+                        break;
+
+                    case "LAMP OFF":
+                        if (called)
+                        {
+                            WebRequest request = WebRequest.Create("http://192.168.1.147/toggleSwitch?sensor=Table%20Lamp&state=False");
+                            WebResponse response = request.GetResponse();
+                            called = false;
+                        }
+                        break;
+
+                    case "PRINTER ON":
+                        if (called)
+                        {
+                            WebRequest request = WebRequest.Create("http://192.168.1.147/toggleSwitch?sensor=3D%20Printer&state=True");
+                            WebResponse response = request.GetResponse();
+                            called = false;
+                        }
+                        break;
+
+                    case "PRINTER OFF":
+                        if (called)
+                        {
+                            WebRequest request = WebRequest.Create("http://192.168.1.147/toggleSwitch?sensor=3D%20Printer&state=False");
+                            WebResponse response = request.GetResponse();
+                            called = false;
+                            called = false;
+                        }
+                        break;
+                    case "COMPUTER":
+                        called = true;
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Active Kinect sensor.
+        /// </summary>
+        private KinectSensor sensor;
+
+        /// <summary>
+        /// Speech recognition engine using audio data from Kinect.
+        /// </summary>
+        private SpeechRecognitionEngine speechEngine;
+
+        /// <summary>
+        /// Gets the metadata for the speech recognizer (acoustic model) most suitable to
+        /// process audio from Kinect device.
+        /// </summary>
+        /// <returns>
+        /// RecognizerInfo if found, <code>null</code> otherwise.
+        /// </returns>
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            foreach (RecognizerInfo recognizer in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                string value;
+                recognizer.AdditionalInfo.TryGetValue("Kinect", out value);
+                if ("True".Equals(value, StringComparison.OrdinalIgnoreCase) && "en-US".Equals(recognizer.Culture.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    return recognizer;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Kinect start tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        public void KinectStart()
+        {
+            called = false;
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+            RecognizerInfo ri = GetKinectRecognizer();
+
+            if (null != ri)
+            {
+                this.speechEngine = new SpeechRecognitionEngine(ri.Id);
+
+                // Load grammar from XML file
+                using (var memoryStream = new MemoryStream(Encoding.ASCII.GetBytes(Properties.Resources.SpeechGrammar)))
+                {
+                    var g = new Grammar(memoryStream);
+                    speechEngine.LoadGrammar(g);
+                }
+
+                speechEngine.SpeechRecognized += SpeechRecognized;
+
+                // For long recognition sessions (a few hours or more), it may be beneficial to turn off adaptation of the acoustic model. 
+                // This will prevent recognition accuracy from degrading over time.
+                ////speechEngine.UpdateRecognizerSetting("AdaptationOn", 0);
+
+                speechEngine.SetInputToAudioStream(
+                    sensor.AudioSource.Start(), new SpeechAudioFormatInfo(EncodingFormat.Pcm, 16000, 16, 1, 32000, 2, null));
+                speechEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        /// <summary>
+        /// Execute Kinect stop tasks
+        /// </summary>
+        /// <param name="sender">object sending the event.</param>
+        /// <param name="e">event arguments.</param>
+        public void KinectStop()
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.AudioSource.Stop();
+
+                this.sensor.Stop();
+                this.sensor = null;
+            }
+
+            if (null != this.speechEngine)
+            {
+                this.speechEngine.SpeechRecognized -= SpeechRecognized;
+                this.speechEngine.RecognizeAsyncStop();
+            }
+        }
+
+        public bool called { get; set; }
+    }
+}
---
+++ c#/KinectService/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+
+namespace KinectService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new KinectVoiceService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
