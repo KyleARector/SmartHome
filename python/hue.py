021d9039a0b537b175211633c0677958b0e07d11
Kyle Rector <kylearector@outlook.com>
2016-10-30 14:54:51 -0400

    Hue interface updates

--- python/hue.py
+++ python/hue.py
@@ -2,9 +2,10 @@ import requests
 
 
 class HueInterface(object):
-    def __init__(self, username):
+    def __init__(self, username, address):
         # Load Hue username on instantiation
         self.username = username
+        self.endpt = address + "/api/"
 
     # Discover new Hue bulbs and return list of ID, name, and type
     # Static method? Return list of addresses?
@@ -13,9 +14,30 @@ class HueInterface(object):
         return hue_bulbs
 
     # Convert hex color codes to hue/saturation value
-    def hex_to_hue_sat(hex):
+    def hex_to_hue_sat(self, hex):
         hue_spec = {"hue": 0, "sat": 0}
         return hue_spec
+
+    # Sets color state of lights.
+    # Will probably combine with on/off method
+    def set_light_color(self, color, id):
+        # Color sent as hex from web interface?
+        return True
+
+    # Send on/off command to specific light
+    def light_on_off(self, state, id):
+        # If the state is true, the value to be set is true
+        if state:
+            value = "true"
+        else:
+            value = "false"
+
+        # Create payload to send to bridge
+        data = {"on": value}
+
+        # Send the request to the Hue bridge
+        req = requests.put(self.endpt + self.username + "/" + "lights/" + id,
+                           json=data)
 
 
 def main():
